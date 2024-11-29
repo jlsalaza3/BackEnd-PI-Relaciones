647b386fb5c79d5bc942ee686d4c17b900634a73
jlsalaza3 <jlsalaza3@gmail.com>
2024-11-29 08:42:36 -0500

    se crean carpetas para la admon de soliciudes para el repositorio, el servicio y el controlador, al interior de las mismas todo esta comentado para no gnerar conflictos en la rama develop, antes de enviar estos cambios se le dio play al main para ver que no sacara errores y todo estuvo OK

---
+++ src/main/java/com/example/prueballaves/controladores/AdmonSolicitudesControlador.java
@@ -0,0 +1,13 @@
+/*package com.example.prueballaves.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/empleadobeneficio")
+
+public class AdmonSolicitudesControlador {
+    @Autowired
+
+}*/
---
+++ src/main/java/com/example/prueballaves/repositorios/IRepositorioAdmonSolicitudes.java
@@ -0,0 +1,9 @@
+/*package com.example.prueballaves.repositorios;
+
+import com.example.prueballaves.modelos.EmpleadoBeneficio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRepositorioAdmonSolicitudes extends JpaRepository<EmpleadoBeneficio, Long> {
+}*/
