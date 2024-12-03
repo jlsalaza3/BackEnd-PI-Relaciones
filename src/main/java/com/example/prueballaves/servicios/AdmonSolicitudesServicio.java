1e1f15181034c05fb89a6e2dc8b08f34cb063051
jlsalaza3 <jlsalaza3@gmail.com>
2024-12-03 09:04:53 -0500

    se crean loscontroladores, los servicios y los repositoris de la HU04 se validan connexiones con el bank

--- src/main/java/com/example/prueballaves/servicios/AdmonSolicitudesServicio.java
+++ src/main/java/com/example/prueballaves/servicios/AdmonSolicitudesServicio.java
@@ -1,4 +1,19 @@
-/*package com.example.prueballaves.servicios;
+package com.example.prueballaves.servicios;
 
+import com.example.prueballaves.modelos.EmpleadoBeneficio;
+import com.example.prueballaves.repositorios.IRepositorioAdmonSolicitudes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AdmonSolicitudesServicio {
-}*/
+    @Autowired
+    IRepositorioAdmonSolicitudes iRepositorioAdmonSolicitudes;
+
+    public List<EmpleadoBeneficio> obtenerSolicitudes(){
+        return iRepositorioAdmonSolicitudes.findAll();
+    }
+
+}
