6b6ccbe725aa5a5a6879d9c3f2c508143cfc4de9
Luis-Ospino <luisospinodev@gmail.com>
2024-12-06 15:09:50 -0500

    Primero: Agrego el servicio de obtener los datos de póliza tanto en el paquete de servicios como en el paquete de controladores, dentro de las clases previamente creadas con los servicios de registro. Segundo: Se logra comunicar con prueba unitaria desde el postman aplicando el GET

--- src/main/java/com/example/prueballaves/repositorios/IRepositorioPoliza.java
+++ src/main/java/com/example/prueballaves/repositorios/IRepositorioPoliza.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRepositorioPoliza  extends JpaRepository<Poliza,Long> {
+
 }
--- src/main/java/com/example/prueballaves/servicios/PolizaServicios.java
+++ src/main/java/com/example/prueballaves/servicios/PolizaServicios.java
@@ -5,6 +5,8 @@ import com.example.prueballaves.repositorios.IRepositorioPoliza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PolizaServicios {
 
@@ -17,5 +19,9 @@ public class PolizaServicios {
         }catch (Exception error){
             throw new Exception(error.getMessage());
         }
+    }
+
+    public List<Poliza> obtenerPoliza(){
+        return iRepositorioPoliza.findAll();
     }
 }
